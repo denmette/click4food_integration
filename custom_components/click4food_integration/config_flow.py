6522da54ef7ef2522d0b0ee34bc079026ed808b6
Maarten Casteels <denmette@gmail.com>
2025-01-14 15:52:00 +0000

    feat: Make it possible to update username and password

--- custom_components/click4food_integration/config_flow.py
+++ custom_components/click4food_integration/config_flow.py
@@ -1,10 +1,25 @@
-"""Adds config flow for Blueprint."""
+"""Adds config flow for Click 4 Food."""
 
 from __future__ import annotations
 
+from tkinter import NO
+from typing import Any
+
 import voluptuous as vol
-from homeassistant import config_entries
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.config_entries import (
+    ConfigEntry,
+    ConfigFlow,
+    ConfigFlowResult,
+    OptionsFlow,
+)
+from homeassistant.const import (
+    CONF_PASSWORD,
+    CONF_USERNAME,
+)
+from homeassistant.core import (
+    HomeAssistant,
+    callback,
+)
 from homeassistant.helpers import selector
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
 from slugify import slugify
@@ -18,15 +33,59 @@ from .api import (
 from .const import DOMAIN, LOGGER
 
 
-class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for Blueprint."""
+def _get_data_schema(
+    hass: HomeAssistant, config_entry: ConfigEntry | None = None
+) -> vol.Schema:
+    """Get a schema with default values."""
+    # If tracking click4food or no config entry is passed in
+    if config_entry is None or config_entry.data.get(DOMAIN, False):
+        return vol.Schema(
+            {
+                vol.Required(
+                    CONF_USERNAME,
+                    default=({}).get(CONF_USERNAME, vol.UNDEFINED),
+                ): selector.TextSelector(
+                    selector.TextSelectorConfig(
+                        type=selector.TextSelectorType.TEXT,
+                    ),
+                ),
+                vol.Required(CONF_PASSWORD): selector.TextSelector(
+                    selector.TextSelectorConfig(
+                        type=selector.TextSelectorType.PASSWORD,
+                    ),
+                ),
+            }
+        )
+    # Default values come from config entry
+    return vol.Schema(
+        {
+            vol.Required(
+                CONF_USERNAME, default=config_entry.data.get(CONF_USERNAME)
+            ): selector.TextSelector(
+                selector.TextSelectorConfig(
+                    type=selector.TextSelectorType.TEXT,
+                ),
+            ),
+            vol.Required(
+                CONF_PASSWORD, default=config_entry.data.get(CONF_PASSWORD)
+            ): selector.TextSelector(
+                selector.TextSelectorConfig(
+                    type=selector.TextSelectorType.PASSWORD,
+                ),
+            ),
+        }
+    )
+
+
+class Click4FoodFlowHandler(ConfigFlow, domain=DOMAIN):
+    """Config flow for Click4Food."""
 
     VERSION = 1
 
     async def async_step_user(
         self,
         user_input: dict | None = None,
-    ) -> config_entries.ConfigFlowResult:
+    ) -> ConfigFlowResult:
         """Handle a flow initialized by the user."""
         _errors = {}
         if user_input is not None:
@@ -49,7 +108,7 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                     ## Do NOT use this in production code
                     ## The unique_id should never be something that can change
                     ## https://developers.home-assistant.io/docs/config_entries_config_flow_handler#unique-ids
-                    unique_id=slugify(user_input[CONF_USERNAME])
+                    unique_id=DOMAIN
                 )
                 self._abort_if_unique_id_configured()
                 return self.async_create_entry(
@@ -59,25 +118,17 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        CONF_USERNAME,
-                        default=(user_input or {}).get(CONF_USERNAME, vol.UNDEFINED),
-                    ): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.TEXT,
-                        ),
-                    ),
-                    vol.Required(CONF_PASSWORD): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.PASSWORD,
-                        ),
-                    ),
-                },
-            ),
+            data_schema=_get_data_schema(self.hass),
             errors=_errors,
         )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: ConfigEntry,
+    ) -> Click4FoodOptionsFlowHandler:
+        """Create the options flow."""
+        return Click4FoodOptionsFlowHandler(config_entry)
 
     async def _test_credentials(self, username: str, password: str) -> None:
         """Validate credentials."""
@@ -87,3 +138,29 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             session=async_create_clientsession(self.hass),
         )
         await client.async_get_data()
+
+
+class Click4FoodOptionsFlowHandler(OptionsFlow):
+    """Options flow for Click4Food component."""
+
+    def __init__(self, config_entry: ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Manage the options."""
+        if user_input is not None:
+            # Update config entry with data from user input
+            self.hass.config_entries.async_update_entry(
+                self.config_entry, data=user_input
+            )
+            return self.async_create_entry(
+                title=self.config_entry.title, data=user_input
+            )
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=_get_data_schema(self.hass, config_entry=self.config_entry),
+        )
